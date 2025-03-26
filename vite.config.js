a4332872969301fd3f948f32c8928fbe91ca72ef
Raj1778 <164516744+Raj1778@users.noreply.github.com>
2025-03-26 17:06:07 +0530

    Fixing errors

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // ✅ Use "./" for relative paths
+  base: "/BollyQuiz/", // Change this to match your GitHub repo name
 });
