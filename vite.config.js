d547307ee374127febf36516204c3333d996ce29
Raj1778 <164516744+Raj1778@users.noreply.github.com>
2025-03-26 16:30:57 +0530

    fixed bugs

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/BollyQuiz/", // ✅ Your repository name
+  base: "./", // ✅ Change from "/BollyQuiz/" to "./"
 });
